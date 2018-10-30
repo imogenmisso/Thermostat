5bfd9b6f3a0945a99699b87dc44f7b531e4cecf0
Anouska Hopkins <anouskahopkins@gmail.com>
2018-10-30 14:18:20 +0000

    Added an increase temperature function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,8 +1,16 @@
 describe("Thermostat", function() {
   var thermostat;
 
-  it("should have a starting temp of 20", function() {
+  beforeEach(function() {
     thermostat = new Thermostat();
+  })
+
+  it("should have a starting temp of 20", function() {
     expect(thermostat.temperature).toEqual(20)
   });
+
+  it("increases the temperature by 1", function() {
+    thermostat.increase(1);
+    expect(thermostat.temperature).toEqual(21)
+  })
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,7 @@
 function Thermostat() {
   this.temperature = 20
 };
+
+Thermostat.prototype.increase = function(number) {
+  return (this.temperature += number);
+}
