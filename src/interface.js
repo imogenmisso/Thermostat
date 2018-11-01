37250e6d07a7169422e255b8c0d4ffb0840b7695
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-11-01 09:39:19 +0000

    added api

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
 
+  $('#city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#user-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=23ff2359b73d7f4489793424ab72a71f&units=metric', function(data) {
+      $('#temperature').text((data.main.temp.toFixed()));
+    })
+  })
+
   $('#temperature_up').on('click', function() {
       thermostat.up();
       updateTemperature();
@@ -30,5 +38,6 @@ $(document).ready(function() {
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.currentUsage());
+    $('#centigrade').attr('class', thermostat.currentUsage());
   };
 });
